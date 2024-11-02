40b1f83152e48dc5764a5ba2b65e48b5a9060d05
Sarah Threewits <sarah3wits@gmail.com>
2024-11-02 12:07:07 -0400

    Added navigation bar

---
+++ Cat-egorize/UserProfileView.swift
@@ -0,0 +1,15 @@
+//
+//  HomePage.swift
+//  Cat-egorize
+//
+//  Created by Sarah Threewits on 11/2/24.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
